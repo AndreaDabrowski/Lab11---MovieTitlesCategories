03b31162810d177e9caf8ca64ee8f9fac59672ce
AndreaDabrowski <AnnMDabrowski@gmail.com>
2018-10-25 11:35:13 -0400

    lab 11 push

---
+++ Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Collections;
+
+namespace Lab11
+{
+    class Program
+    {
+       
+        public static Movie walle = new Movie("Wall-e", "Animated");
+        public static Movie theLittleMermaid = new Movie("The Little Mermaid", "Animated");
+        public static Movie StarTrekNemesis = new Movie("Star Trek: Nemesis", "SciFi");
+        public static Movie loveActually = new Movie("Love, Actually", "Drama");
+        public static Movie panicRoom = new Movie("Panic Room", "Horror");
+        public static Movie houseOfWax = new Movie("House of Wax", "Horror");
+        public static Movie howToTrainYourDragon = new Movie("How to Train Your Dragon", "Animated");
+        public static Movie theNotebook = new Movie("The Notebook", "Drama");
+        public static Movie starWars = new Movie("Star Wars Holiday Special", "Horror");
+        public static Movie fiftyShadesOfGray = new Movie("Fifty shades of Gray", "Drama");
+        
+        public static ArrayList movies = new ArrayList() { walle, theLittleMermaid, StarTrekNemesis, loveActually, panicRoom, houseOfWax, howToTrainYourDragon, theNotebook, starWars, fiftyShadesOfGray };
+
+        public static string category, response;//why static?
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welome to the Movie List Application!!!!!!!!!!!!!111!one!");
+            do
+            {
+                Console.WriteLine("Enter a movie category: ");
+
+                category = Console.ReadLine();
+                Validate.IsInputValid(category);//validates category input
+                Validate.CategorizeMovies(category);//inputs category and displays titles
+                
+                Console.WriteLine("Continue? (y/n): ");//prompts user to continue
+                response = Console.ReadLine();
+                Validate.YesOrNo(response);
+
+            }
+            while (Validate.YesOrNo(response));
+            {
+                Console.WriteLine("Goodbye!");
+            }
+            Console.ReadLine();
+            
+        }
+    }
+}
---
+++ Validate.cs
@@ -0,0 +1,113 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab11
+{
+    class Validate
+    {
+        //validates the input so that it isnt null or a number
+        public static string IsInputValid(string info)
+        {
+            while (true)
+            {
+                if (string.IsNullOrEmpty(info) || info.Length > 35)
+                {
+                    Console.WriteLine("That is not correct input, try again");
+
+                }
+
+                foreach (char item in info)
+                {
+                    if (!char.IsLetter(item))
+                    {
+                        Console.WriteLine("You must use alphabetical characters, try again ");
+
+                    }
+                }
+
+                return info;
+            }
+
+        }
+        //input is the movie category, displays each movie based on the category
+        public static void CategorizeMovies(string userInput)
+        {
+            
+                if (userInput.ToLower() == "horror")
+                {
+                    foreach (Movie movie in Program.movies)
+                    {
+                       if (movie.Category =="Horror")
+                       {
+                            Console.WriteLine(movie.Title);
+                       }
+                    }
+                }
+                else if (userInput.ToLower() == "animated")
+                {
+                    foreach (Movie movie in Program.movies)
+                    {
+                        if (movie.Category == "Animated")
+                        {
+                            Console.WriteLine(movie.Title);
+                        }
+                    }
+                }
+                else if (userInput.ToLower() == "scifi" || userInput.ToLower() == "sci fi")
+                {
+                    foreach (Movie movie in Program.movies)
+                    {
+                        if (movie.Category == "SciFi")
+                        {
+                            Console.WriteLine(movie.Title);
+                        }
+                    }
+                }
+                
+                else if (userInput.ToLower() == "drama")
+                {
+                    foreach (Movie movie in Program.movies)
+                    {
+                        if (movie.Category == "Drama")
+                        {
+                            Console.WriteLine(movie.Title);
+                        }
+                    }
+                }
+                else
+                {
+                    Console.WriteLine("That category was not recognized, please choose from Horror, Animated, SciFi, or Drama: ");
+                    //userInput = Console.ReadLine();
+                }
+
+            
+        }
+        //validates the user input as either y or n
+        public static bool YesOrNo(string response)
+        {
+
+            while (true)
+            {
+
+                if (response == "y")
+                {
+                    return true;
+                }
+                else if (response == "n")
+                {
+                    return false;
+                }
+                else
+                {
+                    Console.Write("Invalid input, try (y/n): ");
+                    response = Console.ReadLine().ToLower();//changes variable and loops to top
+
+                }
+            }
+
+        }
+    }
+}
